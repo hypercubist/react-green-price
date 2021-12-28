d684c2f5d2aeb409ed2e90595c7c002841a50b78
hypercubist <qpinguz@gmail.com>
2021-12-28 23:52:51 +0900

    create chart

--- src/routes/Item.tsx
+++ src/routes/Item.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { fetchItem } from "../api";
+import ApexChart from "react-apexcharts";
 
 const Container = styled.main`
   background-color: ${(props) => props.theme.bgColor};
@@ -10,63 +11,77 @@ const Container = styled.main`
   margin: 0 auto;
 `;
 
-interface RouteParams{
-    categoryCode: string;
-    itemCode:string;
-    kindCode:string;
+const Ul = styled.ul``;
+
+const Li = styled.li``;
+
+interface RouteParams {
+  categoryCode: string;
+  itemCode: string;
+  kindCode: string;
 }
 
 interface ItemData {
-    condition: Condition[];
-    data:      Data;
+  condition: Condition[];
+  data: Data;
 }
 
 interface Condition {
-    p_startday:         Date;
-    p_endday:           Date;
-    p_itemcategorycode: string;
-    p_itemcode:         string;
-    p_kindcode:         string;
-    p_productrankcode:  string;
-    p_countycode:       string;
-    p_convert_kg_yn:    string;
-    p_key:              string;
-    p_id:               string;
-    p_returntype:       string;
+  p_startday: Date;
+  p_endday: Date;
+  p_itemcategorycode: string;
+  p_itemcode: string;
+  p_kindcode: string;
+  p_productrankcode: string;
+  p_countycode: string;
+  p_convert_kg_yn: string;
+  p_key: string;
+  p_id: string;
+  p_returntype: string;
 }
 
 interface Data {
-    error_code: string;
-    item:       Item[];
+  error_code: string;
+  item: ItemObj[];
 }
 
-interface Item {
-    itemname:   string;
-    kindname:   string;
-    countyname: string;
-    marketname: string;
-    yyyy:       string;
-    regday:     string;
-    price:      string;
+interface ItemObj {
+  itemname: string;
+  kindname: string;
+  countyname: string;
+  marketname: string;
+  yyyy: string;
+  regday: string;
+  price: string;
 }
 
+function Item() {
+  const { categoryCode, itemCode, kindCode } = useParams<RouteParams>();
+  const { isLoading, data: priceData } = useQuery<ItemData>(
+    [itemCode, kindCode],
+    () => fetchItem(categoryCode, itemCode, kindCode)
+  );
 
-function Item(){
-    const {categoryCode,itemCode,kindCode} = useParams<RouteParams>();
-    const {isLoading, data} = useQuery<ItemData>([itemCode, kindCode],()=>fetchItem(categoryCode, itemCode, kindCode))
-    console.log(data);
-    return (
-        <Container>
-            {isLoading ? (<h1>Loading..</h1>
-            ) : (
-                <div>
-                    <h3>{data?.data.item[0].price}</h3>
-                    <h3>{data?.data.item[0].itemname}</h3>
-                </div>
-            )
-            }
-        </Container>
-        )
+  return (
+    <Container>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: priceData?.data.item
+                .filter((priceObj) => priceObj.countyname === "평균")
+                .map((priceObj) => Number(priceObj.price.replace(",", ""))),
+            },
+          ]}
+          options={{}}
+        />
+      )}
+    </Container>
+  );
 }
 
 export default Item;
